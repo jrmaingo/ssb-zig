90e761f7c5af689acab6e9dc35fe45e65412f84f
Julian Maingot <julianmaingot@gmail.com>
2020-05-26 23:39:43 -0700

    Create the feed ID when identity is created/loaded

--- src/main.zig
+++ src/main.zig
@@ -9,22 +9,74 @@ const CryptoError = error{GenerationFailure};
 const Identity = struct {
     pk: [c.crypto_sign_ed25519_PUBLICKEYBYTES]u8 = [_]u8{0} ** c.crypto_sign_ed25519_PUBLICKEYBYTES,
     sk: [c.crypto_sign_ed25519_SECRETKEYBYTES]u8 = [_]u8{0} ** c.crypto_sign_ed25519_SECRETKEYBYTES,
-};
+    feed_id: [feed_id_len]u8 = [_]u8{0} ** feed_id_len,
 
-// gen a new identity keypair with libsodium
-fn makeIdenity() !Identity {
-    var identity = Identity{};
+    const feed_id_prefix = "@";
+    const feed_id_suffix = ".ed25519";
+    const feed_id_len = feed_id_prefix.len + std.base64.Base64Encoder.calcSize(c.crypto_sign_ed25519_PUBLICKEYBYTES) + feed_id_suffix.len;
 
-    // generate identity key pair
-    const c_res = c.crypto_sign_ed25519_keypair(&identity.pk, &identity.sk);
-    if (c_res != 0) {
-        return CryptoError.GenerationFailure;
+    fn getFeedID(pk: [c.crypto_sign_ed25519_PUBLICKEYBYTES]u8, feed_id: []u8) void {
+        var pk_base64 = [_]u8{0} ** std.base64.Base64Encoder.calcSize(pk.len);
+        std.base64.standard_encoder.encode(&pk_base64, &pk);
+        const feed_id_parts = [_][]const u8{ Identity.feed_id_prefix, &pk_base64, Identity.feed_id_suffix };
+
+        const out_stream = std.io.fixedBufferStream(feed_id).outStream();
+        for (feed_id_parts) |part| {
+            std.debug.warn("part: {}\n", .{part});
+            out_stream.writeAll(part) catch unreachable;
+            // TODO handle out buf of wrong size
+        }
+
+        std.debug.warn("feed_id: {}\n", .{feed_id});
     }
 
-    std.debug.warn("{x}\n", .{identity});
+    fn createWithKeypair(sk: [c.crypto_sign_ed25519_SECRETKEYBYTES]u8, pk: [c.crypto_sign_ed25519_PUBLICKEYBYTES]u8) Identity {
+        var identity = Identity{};
 
-    return identity;
-}
+        std.mem.copy(u8, &identity.sk, &sk);
+        std.mem.copy(u8, &identity.pk, &pk);
+
+        Identity.getFeedID(identity.pk, &identity.feed_id);
+
+        std.debug.warn("{x}\n", .{identity});
+
+        return identity;
+    }
+
+    // gen a new identity keypair with libsodium
+    fn create() !Identity {
+        var identity = Identity{};
+
+        // generate identity key pair
+        const c_res = c.crypto_sign_ed25519_keypair(&identity.pk, &identity.sk);
+        if (c_res != 0) {
+            return CryptoError.GenerationFailure;
+        }
+
+        Identity.getFeedID(identity.pk, &identity.feed_id);
+
+        std.debug.warn("created keypair:\n{x}\n", .{identity});
+
+        return identity;
+    }
+
+    fn createFromSecretKey(sk: [c.crypto_sign_ed25519_SECRETKEYBYTES]u8) !Identity {
+        var pk: [c.crypto_sign_ed25519_PUBLICKEYBYTES]u8 = undefined;
+        const c_res = c.crypto_sign_ed25519_sk_to_pk(&pk, &sk);
+        if (c_res != 0) {
+            // TODO raise error
+            @panic("Unimplemented");
+        }
+
+        var identity = Identity.createWithKeypair(sk, pk);
+
+        Identity.getFeedID(identity.pk, &identity.feed_id);
+
+        std.debug.warn("loaded keypair:\n{x}\n", .{identity});
+
+        return identity;
+    }
+};
 
 // caller frees return buf
 // using defer allocator.free(ssb_path);
@@ -53,23 +105,15 @@ fn loadIdentity(ssb_dir: std.fs.Dir) !Identity {
     const secret_file = try ssb_dir.openFile(secret_file_name, .{});
     defer secret_file.close();
 
-    var identity = Identity{};
+    var sk: [c.crypto_sign_ed25519_SECRETKEYBYTES]u8 = undefined;
 
-    const read_len = try secret_file.readAll(&identity.sk);
-    if (read_len != identity.sk.len) {
+    const read_len = try secret_file.readAll(&sk);
+    if (read_len != sk.len) {
         // TODO raise error
         @panic("Unimplemented");
     }
 
-    const c_res = c.crypto_sign_ed25519_sk_to_pk(&identity.pk, &identity.sk);
-    if (c_res != 0) {
-        // TODO raise error
-        @panic("Unimplemented");
-    }
-
-    std.debug.warn("loaded keypair:\n{x}\n", .{identity});
-
-    return identity;
+    return try Identity.createFromSecretKey(sk);
 }
 
 pub fn main() anyerror!void {
@@ -100,7 +144,7 @@ pub fn main() anyerror!void {
         // TODO check error cases
 
         // gen new Identity and write to file
-        const newIdentity = try makeIdenity();
+        const newIdentity = try Identity.create();
         try saveIdentity(ssb_dir, newIdentity);
         break :loadErr newIdentity;
     };
